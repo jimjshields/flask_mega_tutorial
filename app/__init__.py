5857de3e2e4c865e17bc420fbb54e6b3e29e24f6
jimjshields <jim.j.shields@gmail.com>
2015-01-20 16:09:44 -0500

    added config for user login & openid

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,18 @@ db = SQLAlchemy(app)
 # from the app module - app.py - import views (which will be created by us)
 # import models for dbs - this is created by us
 from app import views, models
+
+import os
+# handle the users' logged in state
+from flask.ext.login import LoginManager
+# provide authentication
+from flask.ext.openid import OpenId
+# base directory of the project
+from config import basedir
+
+# initialize a loginmanager object
+lm = LoginManager()
+# initialize the app w/ that object
+lm.init_app(app)
+# initialize openid object - requires a path to a temp folder where files can be stored
+oid = OpenId(app, os.path.join(basedir, 'tmp'))
