e87402afe42f4d0d476d5714274fc6e0e147317f
jimjshields <jim.j.shields@gmail.com>
2015-01-20 14:57:12 -0500

    added db config

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 # Flask class allows creation of Flask objects
 from flask import Flask
+# sqlalchemy - for working w/ db
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # Flask object of name __name__ (the name of this file)
 app = Flask(__name__)
 # import configuration from config module
 app.config.from_object('config')
+# initalize database w/ sqlalchemy
+db = SQLAlchemy(app)
 
 # from the app module - app.py - import views (which will be created by us)
-from app import views
+# import models for dbs - this is created by us
+from app import views, models
