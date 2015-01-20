370fc61f5846109c7aefc7c3a4afc9bd80973b02
jimjshields <jim.j.shields@gmail.com>
2015-01-20 13:11:23 -0500

    added config

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 # Flask object of name __name__ (the name of this file)
 app = Flask(__name__)
+# import configuration from config module
+app.config.from_object('config')
 
 # from the app module - app.py - import views (which will be created by us)
 from app import views
