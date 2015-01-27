0d8aef4abb01245f7864c9b7de134b92f31595cd
jimjshields <jim.j.shields@gmail.com>
2015-01-27 14:02:57 -0500

    added form for editing profile

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 # Form class from wtf - used to create form objects
 from flask.ext.wtf import Form
 # different field classes to be used in forms
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, TextAreaField
 # function attached to fields to perform automatic
 # validation on data submitted by users
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 # login form class - inherits from Form class
 class LoginForm(Form):
@@ -13,3 +13,7 @@ class LoginForm(Form):
 	openid = StringField('openid', validators=[DataRequired(message="testing")])
 	# field is called remember_me, either True or False
 	remember_me = BooleanField('remember_me', default=False)
+
+class EditForm(Form):
+	nickname = StringField('nickname', validators=[DataRequired()])
+	about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
