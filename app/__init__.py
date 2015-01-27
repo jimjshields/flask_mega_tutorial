0c15ff6e7213babac9ed7c077e1b4badb9d9ae09
jimjshields <jim.j.shields@gmail.com>
2015-01-27 17:08:48 -0500

    added error logging to a file; removed email logging as it doesn't work for gmail

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ from flask.ext.login import LoginManager
 # provide authentication
 from flask.ext.openid import OpenID
 # base directory of the project
-from config import basedir
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 # initialize a loginmanager object
 lm = LoginManager()
@@ -26,6 +26,35 @@ lm.init_app(app)
 lm.login_view = 'login'
 # initialize openid object - requires a path to a temp folder where files can be stored
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+# Sending logging to e-mail
+# if not app.debug:
+# 	import logging
+# 	from logging.handlers import SMTPHandler
+# 	credentials = None
+# 	if MAIL_USERNAME or MAIL_PASSWORD:
+# 		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+# 	mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 
+# 								'no-reply@%s' % (MAIL_SERVER),
+# 								ADMINS,
+# 								'micro blog failure',
+# 								credentials)
+# 	mail_handler.setLevel(logging.ERROR)
+# 	app.logger.addHandler(mail_handler)
+
+if not app.debug:
+	import logging
+	from logging.handlers import RotatingFileHandler
+	# limit the size to 1MB; keep last 10 logs as backups
+	file_handler = RotatingFileHandler(os.path.join(basedir, 'tmp/microblog.log'), 'a', 1 * 1024 * 1024, 10)
+	# custom formatting of the messages
+	# timestamp, logging level, file and line number, log msg and stack trace
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	app.logger.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+	app.logger.info('microblog startup')
+
+
 
 # from the app module - app.py - import views (which will be created by us)
 # import models for dbs - this is created by us
